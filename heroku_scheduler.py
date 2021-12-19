f2ff1fd8a6a86306c5843c659a4e22cd4eafe985
Ilya-Slesarenko <58133590+Ilya-Slesarenko@users.noreply.github.com>
2021-12-20 07:01:22 +1000

    Update heroku_scheduler.py

--- heroku_scheduler.py
+++ heroku_scheduler.py
@@ -1,11 +1,12 @@
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
-from tickers_updater import Updater
+from ranking_part_to_G import RankingClass
 
 def job_function():
     print(f'gathering started... {str(datetime.datetime.now())}')
-    Updater().__init__
+    RankingClass().spreadsheet_forming()
 
 shed = BlockingScheduler()
-shed.add_job(job_function, 'cron', day_of_week='sat', hour=3, minute=3)  # launch on Saturday morning (6 AM Moscow time)
+shed.add_job(job_function, 'cron', day_of_week='mon', hour=3, minute=3)  # launch on Saturday morning (6 AM Moscow time)
+# shed.add_job(job_function, 'cron', day_of_week='sun', hour=3, minute=3)  # launch on Sunday morning (6 AM Moscow time)
 shed.start()
