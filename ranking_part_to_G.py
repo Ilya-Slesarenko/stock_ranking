324a52f2536044f633e4059d1f7f8a34ea1bb77c
Ilya-Slesarenko <58133590+Ilya-Slesarenko@users.noreply.github.com>
2021-12-21 05:30:16 +1000

    Update ranking_part_to_G.py

--- ranking_part_to_G.py
+++ ranking_part_to_G.py
@@ -1,6 +1,6 @@
 ﻿# -*- coding: utf-8 -*-
 
-import logging, random, warnings, httplib2, apiclient.discovery
+import logging, random, warnings, httplib2, apiclient.discovery, time
 import numpy as np
 from scipy.stats import norm
 import pandas as pd
@@ -92,7 +92,8 @@ class RankingClass():
         final_list = [headers]
         print(f'gathering data for {len(self.tickers_list)} tickers')
 
-        for ticker in self.tickers_list[1400:1420]:
+        for ticker in self.tickers_list:
+            time.sleep(2)
             from_yfinance = self.yfinance_data(ticker)
             try:
                 t_info = yf.Ticker(ticker).info
