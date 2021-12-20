fb706bd56105f051fa70db21b9ab3e981452944e
Ilya-Slesarenko <58133590+Ilya-Slesarenko@users.noreply.github.com>
2021-12-21 00:25:21 +1000

    Update ranking_part_to_G.py

--- ranking_part_to_G.py
+++ ranking_part_to_G.py
@@ -92,7 +92,7 @@ class RankingClass():
         final_list = [headers]
         print(f'gathering data for {len(self.tickers_list)} tickers')
 
-        for ticker in self.tickers_list[:15]:
+        for ticker in self.tickers_list:
             from_yfinance = self.yfinance_data(ticker)
             try:
                 t_info = yf.Ticker(ticker).info
