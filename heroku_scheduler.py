0304ece85916d387042edb51efa83d16832cf9bd
Ilya-Slesarenko <58133590+Ilya-Slesarenko@users.noreply.github.com>
2021-12-20 20:31:02 +1000

    Update heroku_scheduler.py

--- heroku_scheduler.py
+++ heroku_scheduler.py
@@ -7,6 +7,6 @@ def job_function():
     RankingClass().spreadsheet_forming()
 
 shed = BlockingScheduler()
-shed.add_job(job_function, 'cron', day_of_week='mon', hour=10, minute=6)  # launch on Saturday morning (6 AM Moscow time)
+shed.add_job(job_function, 'cron', day_of_week='mon', hour=10, minute=35)  # launch on Saturday morning (6 AM Moscow time)
 # shed.add_job(job_function, 'cron', day_of_week='sun', hour=3, minute=3)  # launch on Sunday morning (6 AM Moscow time)
 shed.start()
