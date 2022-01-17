3c6fdbd257d5d8e41aca9d12ee33b0af3ce90082
Ilya-Slesarenko <58133590+Ilya-Slesarenko@users.noreply.github.com>
2022-01-17 19:19:02 +0300

    Add files via upload

---
+++ w2w_total_change.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import logging, random, warnings, httplib2, apiclient.discovery
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+warnings.filterwarnings('ignore')
+pd.options.mode.chained_assignment = None  # default='warn'
+
+
+class RankingClass():
+    def __init__(self):
+        # Получение списка акций  из готового листа Google Sheet
+        self.CREDENTIALS_FILE = 'stock-spreadsheets-9974a749b7e4.json'
+        self.ranking_page = '1C_uAagRb_GV7tu8X1fbJIM9SRtH3bAcc-n61SP8muXg'
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(self.CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
+        httpAuth = credentials.authorize(httplib2.Http())  # Авторизуемся в системе
+        self.service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)  # Выбираем работу с таблицами и 4 версию API
+
+
+    # stock_market fundamental data from yfinance
+    def total_change_calc(self):
+        #reading the updated result
+        results_rank_updated = self.service.spreadsheets().values().batchGet(spreadsheetId=self.ranking_page, ranges='Retro!A:AE', valueRenderOption='FORMATTED_VALUE', dateTimeRenderOption='FORMATTED_STRING').execute()
+        rank_sheet_values_updated = results_rank_updated['valueRanges'][0]['values'][1:]
+
+        fixed_range = []
+        for v in rank_sheet_values_updated:
+            fixed_values = []
+            for i in v:
+                try:
+                    fixed_values.append(int(i.split(',')[0]))
+                except ValueError:
+                    fixed_values.append(i)
+            fixed_range.append(fixed_values)
+
+        headers = results_rank_updated['valueRanges'][0]['values'][:1][0]
+
+        df_1 = pd.DataFrame(fixed_range, columns=headers)
+        df_needed = df_1[['Time_key', 'latest_Close']].groupby(['Time_key']).sum()
+
+        output_dir = f'C:\\Users\\Ilia\\Documents\\Python_projects\\Stock_analysis\\baseline_analysis\\stock_rank\\w2w_trends.xlsx'
+        writer = pd.ExcelWriter(output_dir, engine='xlsxwriter')
+        df_needed.to_excel(writer, sheet_name='result', index=True)
+        writer.save()
+
+if __name__ == '__main__':
+    RankingClass().total_change_calc()
