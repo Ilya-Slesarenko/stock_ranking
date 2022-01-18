e8200687d8fddff1249b2e2649800a2cc7d6f109
Ilya-Slesarenko <58133590+Ilya-Slesarenko@users.noreply.github.com>
2022-01-18 17:10:01 +0300

    Update heroku_scheduler.py

--- heroku_scheduler.py
+++ heroku_scheduler.py
@@ -1,15 +1,19 @@
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 from ranking_part_to_G import RankingClass
+from w2w_total_change import RankingClass
+from parse_insiders_1 import InsidersDeals
 
 def job_function():
     print(f'gathering started... {str(datetime.datetime.now())}')
     RankingClass().spreadsheet_forming()
 
 w2w_job = RankingClass().total_change_calc()
+insiders_parse = InsidersDeals().PerformAll()
 
 shed = BlockingScheduler(timezone="Europe/Moscow")
 
+shed.add_job(insiders_parse, 'cron', day_of_week='sun', hour=3, minute=5)  # 3 and 5, sun (Sunday)
 shed.add_job(job_function, 'cron', day_of_week='sun', hour=5, minute=5)  # 5 and 5, sun (Sunday)
-shed.add_job(job_function, 'cron', day_of_week='tue', hour=17, minute=6)  # 8 and 5, sun (Sunday)
+shed.add_job(w2w_job, 'cron', day_of_week='tue', hour=17, minute=13)  # set to !!! 8 and 5, sun (Sunday)
 shed.start()
