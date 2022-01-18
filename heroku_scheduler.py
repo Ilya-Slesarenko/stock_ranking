1ec488c90a8f99fe68acc0b50d3809d1908f9869
Ilya-Slesarenko <58133590+Ilya-Slesarenko@users.noreply.github.com>
2022-01-19 08:25:07 +1000

    Update heroku_scheduler.py

--- heroku_scheduler.py
+++ heroku_scheduler.py
@@ -6,11 +6,15 @@ from parse_insiders_1 import InsidersDeals
 
 shed = BlockingScheduler(timezone="Europe/Moscow")
 
-update_spreadsheet = RankingClass().spreadsheet_forming()
+# update_spreadsheet = RankingClass().spreadsheet_forming()
+def job_function():
+    print(f'gathering started... {str(datetime.datetime.now())}')
+    RankingClass().spreadsheet_forming()
+    
 w2w_job = RankingClass().total_change_calc()
 insiders_parse = InsidersDeals().PerformAll()
 
-shed.add_job(insiders_parse, 'cron', day_of_week='wed', hour=1, minute=20)  # 3 and 5, sun (Sunday)
-shed.add_job(update_spreadsheet, 'cron', day_of_week='sun', hour=5, minute=5)  # 5 and 5, sun (Sunday)
-shed.add_job(w2w_job, 'cron', day_of_week='wed', hour=1, minute=25)  # set to !!! 8 and 5, sun (Sunday)
+shed.add_job(insiders_parse, 'cron', day_of_week='wed', hour=1, minute=28)  # 3 and 5, sun (Sunday)
+shed.add_job(job_function, 'cron', day_of_week='sun', hour=5, minute=5)  # 5 and 5, sun (Sunday)
+shed.add_job(w2w_job, 'cron', day_of_week='wed', hour=1, minute=31)  # set to !!! 8 and 5, sun (Sunday)
 shed.start()
