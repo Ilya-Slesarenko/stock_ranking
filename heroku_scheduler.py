f3d0f3d6d744f6c0d2f620c67c73bf84546ad840
Ilya-Slesarenko <58133590+Ilya-Slesarenko@users.noreply.github.com>
2022-01-19 09:53:08 +1000

    Update heroku_scheduler.py

--- heroku_scheduler.py
+++ heroku_scheduler.py
@@ -17,5 +17,5 @@ def job_function_3():
 
 shed.add_job(job_function_1, 'cron', day_of_week='sun', hour=2, minute=5)  # sun (Sunday night)
 shed.add_job(job_function_2, 'cron', day_of_week='sun', hour=5, minute=50)  # sun (Sunday - Monday morning)
-shed.add_job(job_function_3, 'cron', day_of_week='wed', hour=2, minute=47)  # set to !!! 8 and 5, sun (Sunday)
+shed.add_job(job_function_3, 'cron', day_of_week='wed', hour=2, minute=55)  # set to !!! 8 and 5, sun (Sunday)
 shed.start()
