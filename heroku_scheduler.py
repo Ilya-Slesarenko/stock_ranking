bc821904f66ec5e64ccc4000d63d313a5292eb42
Ilya-Slesarenko <58133590+Ilya-Slesarenko@users.noreply.github.com>
2022-02-16 00:44:55 +1000

    Update heroku_scheduler.py

--- heroku_scheduler.py
+++ heroku_scheduler.py
@@ -1,21 +1,20 @@
-import datetime
+import datetime, time
 from apscheduler.schedulers.blocking import BlockingScheduler
 from ranking_part_to_G import RankingClass as scf
-from w2w_total_change import RankingClass as wtc
-from parse_insiders_1 import InsidersDeals
-
-shed = BlockingScheduler(timezone="Europe/Moscow")
+from rank_G_double_check import RankingClass as scf_double_check
+from ranking_part_to_G_2 import RankingClass as scf_2
+# from parse_insiders_1 import InsidersDeals
 
 def job_function_1():
     scf().spreadsheet_forming()
+    time.sleep(10)
+    scf_double_check().spreadsheet_forming_after_check()
+    time.sleep(10)
+    scf_2().PerformAll() # perform the 2-nd part right after the first one
 
-def job_function_2():
-    wtc().total_change_calc()
-    
-def job_function_3():
-    InsidersDeals().PerformAll()
-
-shed.add_job(job_function_1, 'cron', day_of_week='sun', hour=2, minute=5)  # sun (Sunday night : 2 ; 5)
-shed.add_job(job_function_2, 'cron', day_of_week='sun', hour=10, minute=10)  # sun (Sunday - Monday morning)
-# shed.add_job(job_function_3, 'cron', day_of_week='wed', hour=16, minute=24)  # set to !!! 8 and 5, sun (Sunday)
+shed = BlockingScheduler(timezone="Europe/Moscow")
+      
+shed.add_job(job_function_1, 'cron', day_of_week='sun', hour=5, minute=5) 
+# shed.add_job(job_function_2, 'cron', day_of_week='sun', hour=10, minute=5)  # sun (Sunday morning (say, 10 AM)
+# shed.add_job(job_function_3, 'cron', day_of_week='mon', hour=16, minute=37)  # sun (Monday morning)
 shed.start()
